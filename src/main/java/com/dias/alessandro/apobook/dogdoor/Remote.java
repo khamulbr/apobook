962766c53eb65f40a43398d9ae00718667ca690f
Alessandro Dias <alessandro.valerio.dias@gmail.com>
2019-03-31 23:17:26 -0300

    adjustments to close automatically

--- src/main/java/com/dias/alessandro/apobook/dogdoor/Remote.java
+++ src/main/java/com/dias/alessandro/apobook/dogdoor/Remote.java
@@ -1,5 +1,8 @@
 package com.dias.alessandro.apobook.dogdoor;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Remote {
 
     private DogDoor door;
@@ -14,6 +17,16 @@ public class Remote {
             door.close();
         } else {
             door.open();
+
+            final Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    door.close();
+                    timer.cancel();
+                }
+            }, 5000);
+
         }
     }
 
--- src/test/java/com/dias/alessandro/apobook/dogdoor/DogDoorSimulator.java
+++ src/test/java/com/dias/alessandro/apobook/dogdoor/DogDoorSimulator.java
@@ -9,12 +9,16 @@ public class DogDoorSimulator {
         System.out.println("Fido late para sair");
         remote.pressButton();
         System.out.println("\nFido saiu");
-        remote.pressButton();
         System.out.println("\nFido ja terminou");
+        try {
+            Thread.currentThread().sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("\nMas ele esta preso la fora");
+        System.out.println("\nEle comeca a latir");
+        System.out.println("\nEntao Gina pega o controle remoto");
         remote.pressButton();
         System.out.println("\nFido voltou para dentro");
-        remote.pressButton();
     }
-
-
 }
